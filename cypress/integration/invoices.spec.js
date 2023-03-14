43aab091841aa333e45fcb5b9479d27d6221f795
HENDEL SAMY <50583442+SamDz16@users.noreply.github.com>
2023-03-14 11:24:12 +0100

    project initialisation with create-react-app

--- cypress/integration/invoices.spec.js
+++ cypress/integration/invoices.spec.js
@@ -1,4 +1,4 @@
-/// <reference types="Cypress" />
+// <reference types="Cypress" />
 
 import { API_URL, API_KEY, BASE_URL, resetDatabase } from "../utils";
 
--- cypress/utils.js
+++ cypress/utils.js
@@ -1,10 +1,10 @@
 // L'URL sur laquelle votre application web est visitable (à changer si nécessaire)
-export const BASE_URL = "http://localhost:4200/";
+export const BASE_URL = "http://localhost:3000/";
 // L'URL de l'API Supabase à mettre à jour absolument
-export const API_URL = "https://gdzprwugmxdxnpgztbwt.supabase.co/rest/v1";
+export const API_URL = "https://kqtwiqsndgwpxnzndjlw.supabase.co/rest/v1";
 // La clé d'API de votre compte Supabase à mettre à jour absolument
 export const API_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MDUyMjE4MCwiZXhwIjoxOTU2MDk4MTgwfQ.bs4zHK_Hor5Z1ojmZ9MBF_jTvZpiye_DpiUPZcq209A";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtxdHdpcXNuZGd3cHhuem5kamx3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzg3ODgzNjcsImV4cCI6MTk5NDM2NDM2N30.l59i8Kiao4xCL24htpg96FMtR7gfpXKjER3kRwchBic";
 
 /**
  * Petite fonction utilitaire qui permet de supprimer tout ce qui se trouve dans les tables customers et invoices
